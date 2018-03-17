be708983f112a2b32c25a9f8b8f36cea97aa2cc0
cibobo <cibobo2005@gmail.com>
2018-03-17 23:22:16 +0100

    Update BinanceRestLib.py
    Remove the Key

--- BinanceRestLib.py
+++ BinanceRestLib.py
@@ -12,9 +12,6 @@ import hashlib
 # need to use python-requests instead of the dumm original urlopen function
 import requests
 
-APIKey = '4Nn509LkdkK3X3akxXi0yZIaxVKUOOeBbsZFeR2GNvcfN9qoHz9o'
-privateKey = 'TxvqzJzXOPr5BdwIclESOrF38sITgLdPPxuNyNYdk2PfmUVThKFi'
-
 # basic test with timestamp
 baseUrl = 'https://api.binance.com'
 
