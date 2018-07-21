a0dd1f898a195b8b531d701d3b543ff60c30e533
cibobo <cibobo2005@gmail.com>
2018-07-21 17:48:53 +0200

    Config file

--- RunTriangleStrategy.py
+++ RunTriangleStrategy.py
@@ -9,16 +9,39 @@ ref_coin = [['BTC', 'ETH'], ['ETH','BNB'], ['BTC','BNB']]
 # target coin symbol
 symbol = 'BNB'
 
+def isLocalStart():
+    result = False
+
+    fileIn = open("Config.txt",'r')
+    config = fileIn.readline()
+    fileIn.close()
+
+    fileOut = open("Config.txt",'w')
+    fileOut.write("pause")
+    fileOut.close()
+
+    if config != "pause":
+        result = True
+    
+    return result, config
+
+
 begin_time = time.time()
 
 tradInstance = TriangleStrategy.TriangleStrategy(symbol,ref_coin[0]) 
 print("Begin Triangle Trading @", int(time.time()*1000)+tradInstance.time_offset)
 
 while True:
-    isStart = tradInstance.isRemoteStart()
+    # isStart = tradInstance.isRemoteStart()
+    # isStart = True
+
+    isStart, config = isLocalStart()
 
     if isStart:
-        print("Begin triangle trading")
+        inputs = config.split(",")
+        tradInstance.symbol = str(inputs[0])
+        tradInstance.trading_times = int(inputs[1])
+        print("Begin triangle trading with %s for %i times" %(tradInstance.symbol, tradInstance.trading_times))        
         tradInstance.runTriangleStrategy()
     else:
         print("Waiting for the remote control")
